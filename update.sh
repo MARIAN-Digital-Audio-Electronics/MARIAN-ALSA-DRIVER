1bab5b44862b5818fecf474eff4f591b5c479847
Tobias Groß <theguy@audio-fpga.com>
2023-09-01 18:41:10 +0200

    Pulseaudio needs to be stopped before the module can be removed.

--- update.sh
+++ update.sh
@@ -3,8 +3,12 @@
 make clean
 clear
 if make -j4; then
+	systemctl --user stop pulseaudio.socket
+	systemctl --user stop pulseaudio.service
 	sudo rmmod snd-marian
 	sudo insmod marian/snd-marian.ko
+	systemctl --user start pulseaudio.service
+	systemctl --user start pulseaudio.socket
 else
 	echo "Build failed"
 fi
